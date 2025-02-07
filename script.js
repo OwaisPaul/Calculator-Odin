071bb693137f3adc3d64eb50e90595eb546e7648
OwaisPaul <152506988+OwaisPaul@users.noreply.github.com>
2025-02-07 20:07:49 +0530

    2nd part

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const display = document.getElementById('display');
+
+
+ function inTheDisplay(input){
+    display.value += input;
+}
+
+function calculate(){
+
+}
+
+function clearDisplay(){
+    display.value = '';
+}
