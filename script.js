40d9cb2374e35a921c3032050c5f5deaac2ee553
OwaisPaul <152506988+OwaisPaul@users.noreply.github.com>
2025-02-10 18:27:39 +0530

    redesign

--- script.js
+++ script.js
@@ -1,15 +1,68 @@
 
+let num1;
+let operator;
+let num2;
+
+function operate(num1,operator,num2){
+   
+      if (operator === '+') {
+        return add(num1,num2);
+    } else if(operator === '-'){
+        return subtract(num1,num2);
+    } else if(operator === '*'){
+        return multiply(num1,num2);
+    } else if(operator === '/'){
+        if(num2 === 0){
+            return "Cannot divide by zero";
+        }
+        return divide(num1,num2);
+    } else {
+        return "Invalid operator";
+    }
+}
+
 const display = document.getElementById('display');
 
 
- function inTheDisplay(input){
-    display.value += input;
-}
-
-function calculate(){
-
-}
-
 function clearDisplay(){
     display.value = '';
+    
 }
+
+function inTheDisplay(num1,operator,num2) {
+    display.value += num1,operator,num2;
+}
+
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
