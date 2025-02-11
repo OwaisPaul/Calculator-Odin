1caaddc2de6d321f565a6cc50c86240065ee7ee0
OwaisPaul <152506988+OwaisPaul@users.noreply.github.com>
2025-02-11 20:35:52 +0530

    new design

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
-
-let num1;
-let operator;
-let num2;
-
-function operate(num1,operator,num2){
-   
-      if (operator === '+') {
-        return add(num1,num2);
-    } else if(operator === '-'){
-        return subtract(num1,num2);
-    } else if(operator === '*'){
-        return multiply(num1,num2);
-    } else if(operator === '/'){
-        if(num2 === 0){
-            return "Cannot divide by zero";
-        }
-        return divide(num1,num2);
-    } else {
-        return "Invalid operator";
-    }
-}
-
 const display = document.getElementById('display');
 
+let num1;
+let num2;
+let operator;
 
-function clearDisplay(){
+function clearDisplay() {
     display.value = '';
-    
 }
 
-function inTheDisplay(num1,operator,num2) {
-    display.value += num1,operator,num2;
+function onDisplay(num1,operator,num2){
+    display.value = num1,operator,num2;
 }
 
 function add(a,b){
@@ -44,25 +23,28 @@ function subtract(a,b){
 function multiply(a,b){
     return a * b;
 }
-
 function divide(a,b){
     return a / b;
 }
 
+function operate(num1,operator,num2){
+    switch(operator){
+        case '+':
+            return add(num1,num2);
+            break;
+        case '-':
+            return subtract(num1,num2);
+            break;
+        case '*':
+            return multiply(num1,num2);
+            break;
+        case '/': 
+            return divide(num1,num2);
+            break;
+        default:
+            return 'Error';
+
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
